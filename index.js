6ea5d275371d760c54a37e40d7b18e9dabf90df3
pkm774 <mprabhat774@gmail.com>
2024-08-14 20:55:16 +0530

    Implement Blog View & Improve Blog Create
    * Refresh Page after blog creation
    * Refresh page after 30 secs
    
    Signed-off-by: pkm774 <mprabhat774@gmail.com>

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import bodyParser from "body-parser";
 import fs from 'fs';
+//import { v4 as uuidv4 } from 'uuid';
 
 const hostname = '192.168.0.107';
 const port = 8080;
@@ -9,11 +10,34 @@ const app = express();
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-var folderName = '';
-const words = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-
 const d = new Date();
-var date = d.getDate() + '-' + d.getMonth() + '-' + d.getFullYear();
+var blogDate = d.getDate() + '-' + d.getMonth() + '-' + d.getFullYear();
+
+// Declaration
+class BlogData {
+    //constructor(uuid, tittle, name, date, body) {
+    constructor(tittle, name, date, body) {
+        // this.uuid = uuid;
+        this.tittle = tittle;
+        this.name = name;
+        this.date = date;
+        this.body = body;
+    }
+}
+
+var blogs = [];
+let counter = 1;
+
+// Create public/data folder
+if (!fs.existsSync(`public/data`)) {
+    fs.mkdirSync(`public/data`);
+    console.log('Created public/data folder');
+}
+// Create public/data/blogs folder
+if (!fs.existsSync(`public/data/blogs`)) {
+    fs.mkdirSync(`public/data/blogs`);
+    console.log('Created public/data/blogs folder');
+}
 
 app.get('/', (req, res) => {
     res.render('index.ejs');
@@ -24,43 +48,98 @@ app.get('/create', (req, res) => {
 });
 
 app.post('/save', (req, res) => {
-    // Set folder name
-    if (!folderName) {
-        for (var i = 0; i < 8; ++i) {
-            folderName += words[Math.floor(Math.random() * 27)];
-        }
-    }
-
-    // TODO: 2. Post Viewing
-    //       3. Post Update/Delete
-    
-
-    // Create folder
-    try {
-        if (!fs.existsSync(`public/data/blogs/${folderName}`)) {
-            fs.mkdirSync(`public/data/blogs/${folderName}`);
-        }
-    } catch (err) {
-        console.error(err);
-    }
-
     // Create blog file
-    const data = new Uint8Array(Buffer.from(req.body.blogText));
-    fs.writeFile(`public/data/blogs/${folderName}/${date}.txt`, data, (err) => {
-        if (err) throw err;
-    });
+    const fileName = req.body.name;
+    const data = 'Tittle: ' + req.body.tittle + '\nAuthor: ' + fileName + '\nDate: ' + blogDate + '\n\n' + req.body.blog;
 
-    res.sendStatus(201);
+    // Check if the file already exists
+    let newFileName = fileName;
+    // Check if file with same name exist
+    while (fs.existsSync(`public/data/blogs/${newFileName}.txt`)) {
+        // Yes: Create new file with _num
+        newFileName = `${fileName}_${counter}`;
+        counter++;
+    }
+
+    fs.writeFile(`public/data/blogs/${newFileName}.txt`, data, (err) => {
+        if (err) {
+            console.error(err);
+            res.status(500).send('Internal server error');
+        } else {
+            // OK Header Sent
+            res.sendStatus(201);
+        }
+    });
 });
 
-app.get('/blogs', (req, res) => {
-    res.render('blogs.ejs');
+app.get('/blogs', async (req, res) => {
+    try {
+        const files = await fs.promises.readdir('public/data/blogs');
+        blogs = [];
+
+        for (const file of files) {
+            const filePath = `public/data/blogs/${file}`;
+            const fileContent = await fs.promises.readFile(filePath, 'utf8');
+
+            const lines = fileContent.split(/\r\n|\n/);
+            const title = lines[0].split(':')[1].trim();
+            const author = lines[1].split(':')[1].trim();
+            const date = lines[2].split(':')[1].trim();
+            const body = lines.slice(4).join('\n');
+            // const uuid = uuidv4();
+
+            // const blog = new BlogData(uuid, title, author, date, body);
+            const blog = new BlogData(title, author, date, body);
+            blogs.push(blog);
+        }
+        res.render('blogs.ejs', { allBlogs: blogs });
+    } catch (error) {
+        console.error('Error reading files:', error);
+        res.status(500).send('Internal server error');
+    }
+});
+
+// Using dynamic route '/view/:blogId' for varying values for the blogId parameter.
+/*app.get('/view/:blogId', async (req, res) => {
+    // Extract the value of blogId from the URL and makes it available in the req.params object.
+    const blogId = req.params.blogId;
+    try {
+        // Fetch blog data based on blogId (from your data source)
+        const blogData = await fetchBlogData(blogId);
+        // Render the edit page with the retrieved data
+        res.render('view.ejs', { blogTitle: blogData.title, blogBody: blogData.body });
+    } catch (error) {
+        console.error('Error fetching blog data:', error);
+        res.status(500).send('Internal server error');
+    }
+});*/
+
+// Using query parameters to pass additional information as key-value pairs in the URL.
+app.get('/view', (req, res) => {
+    // Extract the blog post ID from the query parameter
+    const postId = req.query.postId;
+
+    // check the corresponding blog post data
+    if (!blogs[postId]) {
+        // Handle invalid post ID (e.g., show an error page)
+        res.status(404).send('Blog post not found');
+        return;
+    }
+
+    // Render the view.ejs template and pass the blog post data
+    res.render('view.ejs', { blog: blogs[postId] });
+});
+
+app.post('/delete', (req, res) => {
+    // TODO: 3. Post Update/Delete
+    res.sendStatus(201);
+    res.render('create.ejs');
 });
 
 app.get('/about', (req, res) => {
     res.render('about.ejs');
 });
 
-app.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+app.listen(process.env.PORT || 8080, () => {
+    console.log(`Server running at ${process.env.PORT}/`);
 });
--- public/scripts/util.js
+++ public/scripts/util.js
@@ -1,14 +1,44 @@
-$("#blogButton").on('click', async (event) => {
+// Create Blog using POST
+$("#createButton").on('click', async (event) => {
     event.preventDefault();
 
     // Get blog text
-    let blogData = $("#inputBox").val();
+    let bloggerName = $("#nameInputBox").val();
+    let bloggingTittle = $("#tittleInputBox").val();
+    let bloggingData = $("#blogInputBox").val();
 
-    if ((blogData.length > 0) && event.type === 'click') {
+    if (!bloggerName && !bloggerName && !bloggingData) {
+        alert("Create a Blog to save !");
+        return;
+    } else if (!bloggerName) {
+        alert("Please write yor name !");
+        return;
+    } else if (!bloggingTittle) {
+        alert("Please write blogging tittle !");
+        return;
+    } else if (!bloggingData) {
+        alert("Please write your blog !");
+        return;
+    }
+
+    let data = {
+        name: bloggerName,
+        tittle: bloggingTittle,
+        blog: bloggingData
+    };
+
+    if (event.type === 'click' && bloggerName && bloggingTittle && bloggingData) {
         // Send blog text
-        $.post('/save', { blogText: blogData });
-        alert('Your Blog has been saved successfully!');
-    } else {
-        alert('Please write something to post!');
+        $.post('/save', data)
+            .done((data, textStatus, jqXHR) => {
+                if (jqXHR.status === 201) {
+                    alert('Blog post created successfully', data);
+                    // Refresh the page
+                    window.location.reload(false); // true for a complete server refresh
+                }
+            })
+            .fail((jqXHR, textStatus, errorThrown) => {
+                console.error('Error creating blog post:', errorThrown);
+            });
     }
 });
