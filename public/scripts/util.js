aa93418601639bd9e935663ba8567256d1eed513
pkm774 <mprabhat774@gmail.com>
2025-08-24 20:50:07 +0530

    Version 4.5
    * CRUD for registered users only
    * Google authentication
    * Option to save session for long or short
    * Ui/Ux improvements
    
    Signed-off-by: pkm774 <mprabhat774@gmail.com>

--- public/scripts/util.js
+++ public/scripts/util.js
@@ -1,202 +1,171 @@
 // Create Blog using POST
 // 'id' Selector for single button
 $(document).ready(function () {
-    const toolbarOptions = [
-        ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
-        ['blockquote', 'code-block'],
-        ['link', 'image', 'video', 'formula'],
-        [{ 'header': 1 }, { 'header': 2 }],               // custom button values
-        [{ 'list': 'ordered' }, { 'list': 'bullet' }, { 'list': 'check' }],
-        [{ 'script': 'sub' }, { 'script': 'super' }],      // superscript/subscript
-        [{ 'indent': '-1' }, { 'indent': '+1' }],          // outdent/indent
-        [{ 'direction': 'rtl' }],                         // text direction
-        [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
-        [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
-        [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
-        [{ 'font': [] }],
-        [{ 'align': [] }],
-        ['clean']                                         // remove formatting button
-    ];
+  const toolbarOptions = [
+    ["bold", "italic", "underline", "strike"], // toggled buttons
+    ["blockquote", "code-block"],
+    ["link", "image", "video", "formula"],
+    [{ header: 1 }, { header: 2 }], // custom button values
+    [{ list: "ordered" }, { list: "bullet" }, { list: "check" }],
+    [{ script: "sub" }, { script: "super" }], // superscript/subscript
+    [{ indent: "-1" }, { indent: "+1" }], // outdent/indent
+    [{ direction: "rtl" }], // text direction
+    [{ size: ["small", false, "large", "huge"] }], // custom dropdown
+    [{ header: [1, 2, 3, 4, 5, 6, false] }],
+    [{ color: [] }, { background: [] }], // dropdown with defaults from theme
+    [{ font: [] }],
+    [{ align: [] }],
+    ["clean"], // remove formatting button
+  ];
 
-    var quill = new Quill('#editor', {
-        theme: 'snow',
-        placeholder: 'Blog Content',
-        modules: {
-            syntax: true,
-            toolbar: toolbarOptions
-        },
-    });
+  var quill = new Quill("#editor", {
+    modules: {
+      syntax: true,
+      toolbar: toolbarOptions,
+    },
+    theme: "snow",
+    placeholder: "Blog Content",
+  });
 
-    // Add a custom handler for the link button
-    quill.getModule('toolbar').addHandler('link', function () {
-        const href = prompt('Enter the link URL');
-        if (href) {
-            // Prepend 'http://' if the URL doesn't include a protocol
-            const fullUrl = /^https?:\/\//i.test(href) ? href : 'http://' + href;
-            const range = this.quill.getSelection();
-            if (range) {
-                this.quill.formatText(range.index, range.length, 'link', fullUrl);
-            }
-        }
-    });
+  function escapeRegExp(string) {
+    //replaces each special character in the string with an escaped version
+    return string.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
+  }
 
-    $("#saveButton").on('click', async (event) => {
-        event.preventDefault();
+  function cleanEditorContent() {
+    const unwantedText =
+      "<p>PlainBashC++C#CSSDiffHTML/XMLJavaJavaScriptMarkdownPHPPythonRubySQL</p>";
 
-        let bloggerName = $("#nameInputBox").val();
-        let bloggingTitle = $("#titleInputBox").val();
-        let bloggerEmail = $("#emailInputBox").val();
-        let bloggingData = quill.root.innerHTML;  // Get the HTML content from Quill
-        console.log(bloggerEmail);
+    const escapedText = escapeRegExp(unwantedText);
+    // create the regular expression
+    const regex = new RegExp(escapedText, "g");
+    let editorHtml = quill.root.innerHTML;
+    editorHtml = editorHtml.replace(regex, "");
+    quill.root.innerHTML = editorHtml;
+  }
 
-        // Function to check for forbidden characters
-        function containsForbiddenChars(value) {
-            // check for '..' '/' '\'
-            const forbiddenChars = /(\.\.|\/|\\)/;
-            return forbiddenChars.test(value);
-        }
+  cleanEditorContent();
 
-        // Validate bloggerName for forbidden characters
-        if (containsForbiddenChars(bloggerName)) {
-            alert("Unsupported characters in blogger name!");
-            return;
-        }
+  // Add a custom handler for the link button
+  quill.getModule("toolbar").addHandler("link", function () {
+    const href = prompt("Enter the link URL");
+    if (href) {
+      // Prepend 'http://' if the URL doesn't include a protocol
+      const fullUrl = /^https?:\/\//i.test(href) ? href : "http://" + href;
+      const range = this.quill.getSelection();
+      if (range) {
+        this.quill.formatText(range.index, range.length, "link", fullUrl);
+      }
+    }
+  });
 
-        if (!bloggingTitle && !bloggingData && !bloggerName) {
-            alert("Create a Blog to save !");
-            return;
-        } else if (!bloggingTitle) {
-            alert("Blog title is required !");
-            return;
-        } else if (!bloggingData) {
-            alert("Blog should not be empty !");
-            return;
-        } else if (!bloggerName) {
-            alert("Author name is required !");
-            return;
-        }
-
-        let data = {
-            title: bloggingTitle,
-            blog: bloggingData,
-            name: bloggerName,
-            email: bloggerEmail
-        };
-
-        $.post('/save', data)
-            .done((data, textStatus, jqXHR) => {
-                if (jqXHR.status === 201) {
-                    alert('Blog post created successfully');
-                    window.location.href = '/home';
-                }
-            })
-            .fail((jqXHR, textStatus, errorThrown) => {
-                console.error('Error creating blog post:', errorThrown);
-            });
-    });
-
-    // Append Blog using POST
-    // 'id' Selector for single button
-    $("#appendButton").on('click', async (event) => {
-        event.preventDefault();
-
-        let bloggingTitle = $("#formControlInput2").val();
-        let bloggingBody = quill.root.innerHTML;
-
-        if (bloggingBody === 'NaN' || bloggingTitle === 'NaN' || (bloggingBody === 'NaN' && bloggingTitle === 'NaN')) {
-            alert("Redirecting to home page again");
-            window.location.href = '/home';
-            return;
-        } else if (!bloggingBody || !bloggingTitle || (!bloggingBody && !bloggingTitle)) {
-            alert("Input Fields are Empty !");
-            window.location.href = '/home';
-            return;
-        }
-
-        let data = {
-            title: bloggingTitle,
-            blog: bloggingBody
-        };
-
-        if (event.type === 'click' && bloggingBody) {
-            // Get the clicked button's ID
-            const blogId = $("#appendButton").attr('blogId');
-
-            // Send blog text
-            let url = `/append?blogId=${blogId}`;
-
-            $.ajax({
-                url: url,
-                type: 'PATCH',
-                data: JSON.stringify(data),
-                contentType: 'application/json',
-                success: function (data, textStatus, jqXHR) {
-                    if (jqXHR.status === 200) {
-                        alert('Client: Blog post updated successfully');
-                        window.location.href = '/home';
-                    }
-                },
-                error: function (jqXHR, textStatus, errorThrown) {
-                    console.error('Client: Error updating blog post:', errorThrown);
-                }
-            });
-        }
-    });
-});
-/*$("#saveButton").on('click', async (event) => {
+  $("#saveButton").on("click", async (event) => {
     event.preventDefault();
 
-    // Get blog text
     let bloggerName = $("#nameInputBox").val();
-    let bloggingTittle = $("#titleInputBox").val();
-    let bloggingData = $("#blogInputBox").val();
+    let bloggingTitle = $("#titleInputBox").val();
+    let bloggerEmail = $("#emailInputBox").val();
+    let bloggingData = quill.root.innerHTML;
 
     // Function to check for forbidden characters
     function containsForbiddenChars(value) {
-        // check for '..' '/' '\'
-        const forbiddenChars = /(\.\.|\/|\\)/;
-        return forbiddenChars.test(value);
+      // check for '..' '/' '\'
+      const forbiddenChars = /(\.\.|\/|\\)/;
+      return forbiddenChars.test(value);
     }
 
     // Validate bloggerName for forbidden characters
     if (containsForbiddenChars(bloggerName)) {
-        alert("Unsupported characters in blogger name!");
-        return;
+      alert("Unsupported characters in blogger name!");
+      return;
     }
 
-    if (!bloggerName && !bloggerName && !bloggingData) {
-        alert("Create a Blog to save !");
-        return;
-    } else if (!bloggingTittle) {
-        alert("Blog title is required !");
-        return;
+    if (!bloggingTitle && !bloggingData && !bloggerName) {
+      alert("Create a Blog to save !");
+      return;
+    } else if (!bloggingTitle) {
+      alert("Blog title is required !");
+      return;
     } else if (!bloggingData) {
-        alert("Blog should not be empty !");
-        return;
+      alert("Blog should not be empty !");
+      return;
     } else if (!bloggerName) {
-        alert("Author name is required !");
-        return;
+      alert("Author name is required !");
+      return;
     }
 
     let data = {
-        name: bloggerName,
-        tittle: bloggingTittle,
-        blog: bloggingData
+      title: bloggingTitle,
+      blog: bloggingData,
+      name: bloggerName,
+      email: bloggerEmail,
     };
 
-    if (event.type === 'click' && bloggerName && bloggingTittle && bloggingData) {
-        // Send blog text
-        $.post('/save', data)
-            .done((data, textStatus, jqXHR) => {
-                if (jqXHR.status === 201) {
-                    alert('Client: Blog post created successfully', data);
-                    // Refresh the page
-                    // true for a complete server refresh
-                    window.location.reload(false);
-                }
-            })
-            .fail((jqXHR, textStatus, errorThrown) => {
-                console.error('Client: Error creating blog post:', errorThrown);
-            });
+    $.post("/save", data)
+      .done((data, textStatus, jqXHR) => {
+        if (jqXHR.status === 201) {
+          alert("Blog post created successfully");
+          window.location.href = "/home";
+        }
+      })
+      .fail((jqXHR, textStatus, errorThrown) => {
+        console.error("Error creating blog post:", errorThrown);
+      });
+  });
+
+  // Append Blog using POST
+  // 'id' Selector for single button
+  $("#appendButton").on("click", async (event) => {
+    event.preventDefault();
+
+    let bloggingTitle = $("#formControlInput2").val();
+    let bloggingBody = quill.root.innerHTML;
+
+    if (
+      bloggingBody === "NaN" ||
+      bloggingTitle === "NaN" ||
+      (bloggingBody === "NaN" && bloggingTitle === "NaN")
+    ) {
+      alert("Redirecting to home page again");
+      window.location.href = "/home";
+      return;
+    } else if (
+      !bloggingBody ||
+      !bloggingTitle ||
+      (!bloggingBody && !bloggingTitle)
+    ) {
+      alert("Input Fields are Empty !");
+      window.location.href = "/home";
+      return;
     }
-});*/
+
+    let data = {
+      title: bloggingTitle,
+      blog: bloggingBody,
+    };
+
+    if (event.type === "click" && bloggingBody) {
+      // Get the clicked button's ID
+      const blogId = $("#appendButton").attr("blogId");
+
+      // Send blog text
+      let url = `/append?blogId=${blogId}`;
+
+      $.ajax({
+        url: url,
+        type: "PATCH",
+        data: JSON.stringify(data),
+        contentType: "application/json",
+        success: function (data, textStatus, jqXHR) {
+          if (jqXHR.status === 200) {
+            alert("Client: Blog post updated successfully");
+            window.location.href = "/home";
+          }
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+          console.error("Client: Error updating blog post:", errorThrown);
+        },
+      });
+    }
+  });
+});
